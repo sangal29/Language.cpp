748c3884a6bf4109493c80fd7a209a51e87d20e3
sangal29 <sangalrishabh29@gmail.com>
2024-12-14 15:26:39 +0530

    Learning of cpp

---
+++ CODE/program-5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+// Function declaration 
+
+int sum_of_n(int n)
+{
+    int sum = 0;
+    for( int i = 1; i<= n; i++)
+    {
+        sum  += i;
+      
+    }
+    return sum;
+}
+int main()
+{
+    int number;
+    cout<<" Enter the number = ";
+    cin >> number;
+    
+    // function call 
+    int sum = sum_of_n(number);
+    cout << sum;
+    return 0;
+
+}
