5de84c4e0dbcd0bb41a03c0b0d0a4ae75d7cc5ee
sangal29 <sangalrishabh29@gmail.com>
2024-12-22 15:46:27 +0530

    Best Buy sell qution

---
+++ Best buy and sell/Program_1.cpp
@@ -0,0 +1,35 @@
+// # pragma-1 This is the progra for buy and sell  
+
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int maxprofit( vector<int>p , int size)
+{
+    int best_buy =p[0]; 
+    int max_profit = 0;
+
+    for( int i = 1; i < size; i++)
+    {
+        if( p[i]  > best_buy )
+        {
+           max_profit = max( max_profit, p[i] - best_buy);
+        }
+       
+        best_buy = min(best_buy  , p[i]);
+        
+
+    }
+     return max_profit;
+   
+}
+int main()
+{
+    vector<int>price = { 7,1,5,3,6,4};
+    int size = price.size();
+
+    int result = maxprofit(price , size);
+    cout <<" Max profit = " << result;
+    return 0;
+}
